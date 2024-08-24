49afd8e78d56510afbddd9696bf7110293c900a2
azizkayumov <kayumovabduaziz@gmail.com>
2024-08-24 21:47:01 +0900

    fix demo

--- demo/data/plot_tree.py
+++ demo/data/plot_tree.py
@@ -23,12 +23,12 @@ fig, ax = plt.subplots()
 colors = ['r', 'g', 'b', 'y', 'c', 'm']
 for id, (x, y, radius, children) in bubbles.items():
     color = colors[id % len(colors)]
-    circle = plt.Circle((x, y), radius, color=color, alpha=0.25)
+    circle = plt.Circle((x, y), radius, color='b', alpha=0.25)
     ax.add_artist(circle)
     for child in children:
         child = int(child)
         x, y = points[child][0], points[child][1]
-        plt.scatter(x, y, color=color, s=2, marker='+')
+        plt.scatter(x, y, color='b', s=2, marker='+')
 
 # remove coordinate axis names
 plt.xticks([])
@@ -36,12 +36,7 @@ plt.yticks([])
 plt.gca().set_aspect('equal')
 
 title = "N = " + str(len(points)) + ", L = " + str(len(bubbles))
-plt.text(-90, 1120, title, fontsize=8, color='black')
+plt.title(title)
 
 # set axis limits
-feature_max = 1000
-window = 100
-plt.xlim(-window, feature_max + window)
-plt.ylim(-window, feature_max + window) 
-
 plt.savefig("tree.png", dpi=300)
--- demo/src/main.rs
+++ demo/src/main.rs
@@ -3,10 +3,15 @@ use rindex::Rindex;
 use std::io::{BufRead, Write};
 
 fn main() {
-    // Read the spreader dataset
+    // Read the sparse dataset (https://www.kaggle.com/datasets/joonasyoon/clustering-exercises)
     let mut data = Vec::new();
-    let file = std::fs::File::open("demo/data/spreader2D.csv").unwrap();
+    let file = std::fs::File::open("demo/data/sparse.csv").unwrap();
+    let mut skip_header = true;
     for line in std::io::BufReader::new(file).lines() {
+        if skip_header {
+            skip_header = false;
+            continue;
+        }
         let line = line.unwrap();
         let mut iter = line.split(',');
         let x = iter.next().unwrap().parse::<f64>().unwrap();
@@ -15,9 +20,9 @@ fn main() {
     }
 
     // Configure the tree
-    let mut tree = Rindex::default();
+    let mut tree = Rindex::new(5).expect("Invalid fanout");
     let mut point_ids = Vec::new();
-    let deletion_probability = 0.0; // 20%
+    let deletion_probability = 0.25; // 25%
 
     // Perform random insertions and deletions
     let mut rng = StdRng::seed_from_u64(0);
